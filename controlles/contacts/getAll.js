5152ce461189be9c59f202c2478f95d89cb96ce4
volodymyr-oliinyk8 <oliynyk.vo@gmail.com>
2021-11-15 22:15:02 +0200

    completed favorite contacts

--- controlles/contacts/getAll.js
+++ controlles/contacts/getAll.js
@@ -1,14 +1,30 @@
 const { Contact } = require("../../model");
 
 const getAll = async (req, res) => {
+  const { page = 1, limit = 20, favorite } = req.query;
+  const skip = (page - 1) * limit;
   const { _id } = req.user;
-  const contacts = await Contact.find({ owner: _id });
+  if (!favorite) {
+    const contacts = await Contact.find(
+      { owner: _id },
+      "_id name email phone favorite owner",
+      { skip, limit: +limit }
+    ).populate("owner", "email");
+    res.json({
+      status: "success",
+      code: 200,
+      contacts,
+    });
+  }
+  const contacts = await Contact.find(
+    { owner: _id, favorite },
+    "_id name email phone favorite owner",
+    { skip, limit: +limit }
+  ).populate("owner", "email");
   res.json({
     status: "success",
     code: 200,
-    data: {
-      contacts,
-    },
+    contacts,
   });
 };
 
